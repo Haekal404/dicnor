e6bd3afc69eb30fb60918e55a7b947047d46013c
ryu-rhee <ryu-rhee@proton.me>
2023-02-27 15:44:00 +0800

    initial change from `ryu`

--- src/routes/(public)/+page.svelte
+++ src/routes/(public)/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class="flex justify-center items-center min-h-screen flex-col">
+	<p class="font-bold text-4xl">Primary Collaborators</p>
+</section>
