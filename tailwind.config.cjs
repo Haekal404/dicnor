2f647e7e02924ef7a40195ae9ee86e09c2cc2699
ryu-rhee <ryu-rhee@proton.me>
2023-02-28 23:52:51 +0800

    update `tailwind` config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,17 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
+		container: {
+			center: true,
+			padding: {
+				DEFAULT: '1rem',
+				sm: '2rem',
+				md: '3rem',
+				lg: '4rem',
+				xl: '5rem',
+				'2xl': '6rem'
+			}
+		},
 		extend: {
 			fontFamily: {
 				righteous: ['Righteous']
