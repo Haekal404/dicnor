b808c99fd5d93fcbe8d2795ae22380d462350061
ryu-rhee <ryu-rhee@proton.me>
2023-02-28 23:27:07 +0800

    add `font` to projects

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,12 @@
 	import '../app.css';
 </script>
 
-<slot />
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+	<link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet" />
+</svelte:head>
+
+<main class="font-righteous">
+	<slot />
+</main>
