5cced4920abe9a8fe59d37ca0f80e12b7f5140ed
ryu-rhee <ryu-rhee@proton.me>
2023-03-03 21:03:49 +0800

    comment `bootstrap` button

--- src/routes/(public)/+page.svelte
+++ src/routes/(public)/+page.svelte
@@ -1,5 +1,5 @@
 <section class="flex min-h-screen flex-col items-center justify-center">
 	<p class="text-4xl font-bold">Primary Collaborators</p>
 	<!-- NOTE: testing for bootstrap -->
-	<button type="button" class="btn btn-outline-dark">Bobs</button>
+	<!-- <button type="button" class="btn btn-outline-dark">Bobs</button> -->
 </section>
